b3ec4fdc48433aae310ad24210e15ec3e413bd6c
piggy2008 <piggy2008>
2021-12-24 16:15:04 +0900

    add lab branch to compute loss

--- misc.py
+++ misc.py
@@ -56,6 +56,23 @@ class CriterionDSN(nn.Module):
         loss2 = self.criterion(scale_pred, target)
         return self.dsn_weight*loss1 + loss2
 
+class TVLoss(nn.Module):
+    def __init__(self,TVLoss_weight=1):
+        super(TVLoss,self).__init__()
+        self.TVLoss_weight = TVLoss_weight
+
+    def _tensor_size(self, t):
+        return t.size()[1] * t.size()[2] * t.size()[3]
+
+    def forward(self,x):
+        batch_size = x.size()[0]
+        h_x = x.size()[2]
+        w_x = x.size()[3]
+        count_h = self._tensor_size(x[:,:,1:,:])
+        count_w = self._tensor_size(x[:,:,:,1:])
+        h_tv = torch.pow((x[:,:,1:,:]-x[:,:,:h_x-1,:]),2).sum()
+        w_tv = torch.pow((x[:,:,:,1:]-x[:,:,:,:w_x-1]),2).sum()
+        return self.TVLoss_weight*2*(h_tv/count_h+w_tv/count_w)/batch_size
 
 class VGGPerceptualLoss(torch.nn.Module):
     def __init__(self, resize=True):
