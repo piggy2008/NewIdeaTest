a6b2db26f8337cc4c4f18250434f7e7a2d29b120
piggy2008 <piggy2008>
2022-01-17 18:42:07 +0900

    add branch to search lab

--- train_water.py
+++ train_water.py
@@ -59,7 +59,7 @@ args = {
     'iter_num': 240000,
     'iter_save': 4000,
     'iter_start_seq': 0,
-    'train_batch_size': 10,
+    'train_batch_size': 14,
     'last_iter': 0,
     'lr': 1e-4,
     'lr_decay': 0.9,
