074a952e56311ac0a5f6678251a5bec49df6cee4
piggy2008 <piggy2008>
2022-01-17 18:38:19 +0900

    add branch to search lab

--- underwater_model/base_SPOS.py
+++ underwater_model/base_SPOS.py
@@ -66,22 +66,22 @@ class Base(nn.Module):
         super(Base, self).__init__()
         # self.conv1_hsv = nn.Sequential(nn.Conv2d(3, channels[0], kernel_size=3, stride=1, padding=1),
         #                                nn.ReLU(inplace=True))
-        # self.conv1_lab = nn.Sequential(nn.Conv2d(3, channels[0], kernel_size=3, stride=1, padding=1),
-        #                                nn.ReLU(inplace=True))
+        self.conv1_lab = nn.Sequential(nn.Conv2d(3, channels[0], kernel_size=3, stride=1, padding=1),
+                                       nn.ReLU(inplace=True))
         self.conv1_rgb = nn.Sequential(nn.Conv2d(3, channels[0], kernel_size=3, stride=1, padding=1),
                                        nn.ReLU(inplace=True))
 
         # self.conv2_hsv = nn.Sequential(nn.Conv2d(channels[0], channels[1], kernel_size=3, stride=1, padding=1),
         #                                nn.ReLU(inplace=True))
-        # self.conv2_lab = nn.Sequential(nn.Conv2d(channels[0], channels[1], kernel_size=3, stride=1, padding=1),
-        #                                nn.ReLU(inplace=True))
+        self.conv2_lab = nn.Sequential(nn.Conv2d(channels[0], channels[1], kernel_size=3, stride=1, padding=1),
+                                       nn.ReLU(inplace=True))
         self.conv2_rgb = nn.Sequential(nn.Conv2d(channels[0], channels[1], kernel_size=3, stride=1, padding=1),
                                        nn.ReLU(inplace=True))
 
         # self.conv3_hsv = nn.Sequential(nn.Conv2d(channels[1], channels[2], kernel_size=3, stride=1, padding=1),
         #                                nn.ReLU(inplace=True))
-        # self.conv3_lab = nn.Sequential(nn.Conv2d(channels[1], channels[2], kernel_size=3, stride=1, padding=1),
-        #                                nn.ReLU(inplace=True))
+        self.conv3_lab = nn.Sequential(nn.Conv2d(channels[1], channels[2], kernel_size=3, stride=1, padding=1),
+                                       nn.ReLU(inplace=True))
         self.conv3_rgb = nn.Sequential(nn.Conv2d(channels[1], channels[2], kernel_size=3, stride=1, padding=1),
                                        nn.ReLU(inplace=True))
 
@@ -109,7 +109,7 @@ class Base(nn.Module):
     def forward(self, rgb, hsv, lab, select):
         x_rgb = self.conv1_rgb(rgb)
         # x_hsv = self.conv1_hsv(hsv)
-        # x_lab = self.conv1_lab(lab)
+        x_lab = self.conv1_lab(lab)
         # 128 * 128
         x_rgb = self.block1(x_rgb, select[0])
         x_rgb = self.block2(x_rgb, select[1])
@@ -137,14 +137,15 @@ class Base(nn.Module):
         # x_hsv3 = F.max_pool2d(x_hsv3, kernel_size=3, stride=2, padding=1)
 
         # 64 * 64
-        # x_lab = self.block1(x_lab, select[6])
-        # x_lab = self.block2(x_lab, select[7])
-        # x_lab2 = F.max_pool2d(x_lab, kernel_size=3, stride=2, padding=1)
-        # x_lab2 = self.conv2_lab(x_lab2)
-        # x_lab2 = self.block3(x_lab2, select[8])
-        # x_lab2 = self.block4(x_lab2, select[9])
-        # x_lab3 = F.max_pool2d(x_lab2, kernel_size=3, stride=2, padding=1)
-        # x_lab3 = self.conv3_lab(x_lab3)
-        # x_lab3 = self.block5(x_lab3, select[10])
-        # x_lab3 = self.block6(x_lab3, select[11])
-        return x_rgb, x_rgb2, x_rgb3
+        x_lab = self.block1(x_lab, select[6])
+        x_lab = self.block2(x_lab, select[7])
+        x_lab2 = F.max_pool2d(x_lab, kernel_size=3, stride=2, padding=1)
+        x_lab2 = self.conv2_lab(x_lab2)
+        x_lab2 = self.block3(x_lab2, select[8])
+        x_lab2 = self.block4(x_lab2, select[9])
+        x_lab3 = F.max_pool2d(x_lab2, kernel_size=3, stride=2, padding=1)
+        x_lab3 = self.conv3_lab(x_lab3)
+        x_lab3 = self.block5(x_lab3, select[10])
+        x_lab3 = self.block6(x_lab3, select[11])
+        return x_rgb, x_lab, x_rgb2, x_lab2, \
+               x_rgb3, x_lab3
