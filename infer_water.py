6ff0f3b70089bc1d5c4b2a35465022c257ca5e07
piggy2008 <piggy2008>
2022-01-20 11:22:32 +0900

    add a tail to search lab

--- infer_water.py
+++ infer_water.py
@@ -28,21 +28,21 @@ torch.cuda.set_device(device_id)
 # you should have the pth file in the folder './$ckpt_path$/$exp_name$'
 ckpt_path = saving_path
 
-exp_name = 'WaterEnhance_2021-12-23 12:38:02'
+exp_name = 'WaterEnhance_2021-12-23 16:13:35'
 args = {
     'gnn': True,
     'snapshot': '64000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     # 'input_size': (380, 380),
-    # 'image_path': '/mnt/hdd/data/ty2/input_test',
-    # 'depth_path': '/mnt/hdd/data/ty2/depth_test',
-    # 'gt_path': '/mnt/hdd/data/ty2/gt_test',
-    'image_path': '/home/ty/data/uw/input_test',
-    'depth_path': '/home/ty/data/uw/depth_test',
-    'gt_path': '/home/ty/data/uw/gt_test',
+    'image_path': '/mnt/hdd/data/ty2/input_test',
+    'depth_path': '/mnt/hdd/data/ty2/depth_test',
+    'gt_path': '/mnt/hdd/data/ty2/gt_test',
+    # 'image_path': '/home/ty/data/uw/input_test',
+    # 'depth_path': '/home/ty/data/uw/depth_test',
+    # 'gt_path': '/home/ty/data/uw/gt_test',
     'dataset': 'UIEB',
-    'start': 100000
+    'start': 0
 }
 
 img_transform = transforms.Compose([
@@ -94,19 +94,19 @@ def main(snapshot):
 
             # img_list = [i_id.strip() for i_id in open(imgs_path)]
             img = Image.open(os.path.join(args['image_path'], name + '.png')).convert('RGB')
+            img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
             depth = Image.open(os.path.join(args['depth_path'], name + '.png_depth_estimate.png')).convert('L')
 
-            hsv = img.convert('HSV')
-            srgb_profile = ImageCms.createProfile("sRGB")
-            lab_profile = ImageCms.createProfile("LAB")
-
-            rgb2lab_transform = ImageCms.buildTransformFromOpenProfiles(srgb_profile, lab_profile, "RGB", "LAB")
-            lab = ImageCms.applyTransform(img, rgb2lab_transform)
+            hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+            # srgb_profile = ImageCms.createProfile("sRGB")
+            # lab_profile = ImageCms.createProfile("LAB")
+            # rgb2lab_transform = ImageCms.buildTransformFromOpenProfiles(srgb_profile, lab_profile, "RGB", "LAB")
+            lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
             img_var = Variable(img_transform(img).unsqueeze(0), volatile=True).cuda()
             hsv_var = Variable(img_transform(hsv).unsqueeze(0), volatile=True).cuda()
             lab_var = Variable(img_transform(lab).unsqueeze(0), volatile=True).cuda()
             depth_var = Variable(img_transform(depth).unsqueeze(0), volatile=True).cuda()
-            prediction, rgb_side, hsv_side, lab_side = net(img_var, hsv_var, lab_var, depth_var, [3, 3, 3, 3, 3, 3, 3, 3, 3, 3])
+            prediction, rgb_side, hsv_side, lab_side = net(img_var, hsv_var, lab_var, depth_var, [3, 3, 3, 3])
             # prediction = torch.unsqueeze(prediction, 0)
             # print(torch.unique(prediction))
             # precision = to_pil(prediction.data.squeeze(0).cpu())
