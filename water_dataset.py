63efc9cacd4db16cd4d96b514463920b0e4e82e8
piggy2008 <piggy2008>
2022-02-06 13:31:35 +0900

    add segmentation results

--- water_dataset.py
+++ water_dataset.py
@@ -161,14 +161,15 @@ class WaterImage3Folder(data.Dataset):
         lab = convert_from_image_to_lab(img)
         target = convert_from_BGR_to_RGB(img_list[1])
         lab_target = convert_from_image_to_lab(target)
-
+        segmentation = np.concatenate((img_list[2], img_list[3], img_list[4], img_list[5], img_list[6]))
+        print(img.shape, '-', lab.shape, '-', target.shape, '-', segmentation.shape)
         if self.transform is not None:
             img = self.transform(img)
             hsv = self.transform(hsv)
             lab = self.transform(lab)
             target = self.transform(target)
             lab_target = self.transform(lab_target)
-            segmentation = self.transform(img_list[2])
+            segmentation = self.transform(segmentation)
 
         return img, hsv, lab, target, lab_target, segmentation
 
