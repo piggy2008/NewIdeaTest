f8763f2367c553553acf8e81c5b17a57cd030e40
piggy2008 <piggy2008>
2022-03-09 11:54:29 +0900

    add nas trans block

--- underwater_model/choice_block.py
+++ underwater_model/choice_block.py
@@ -1,5 +1,9 @@
 from underwater_model.op import *
 from underwater_model.restormer import TransformerBlock
+from underwater_model.trans_block_dual import TransformerBlock_dual
+from underwater_model.trans_block_eca import TransformerBlock_eca
+from underwater_model.trans_block_sa import TransformerBlock_sa
+from underwater_model.trans_block_sge import TransformerBlock_sge
 class block_choice(nn.Module):
     def __init__(self, n_channels=128):
         super(block_choice, self).__init__()
@@ -14,12 +18,26 @@ class block_choice(nn.Module):
         self.resblock_conv = Conv(n_channels, n_channels, 3, 1, 1, affine=True, upsample=False)
         # self.resblock_vit = ViT(image_size=vit_image_size, patch_size=vit_patch_size, dim=128, depth=1, heads=1, mlp_dim=128, channels=128)
         # self.resblock_convdouble = ConvDouble(n_channels, n_channels, 3, 1, 1, affine=True, upsample=False)
-        # self.resblock_da = DoubleAttentionLayer(n_channels, n_channels, n_channels)
-        self.resblock_da = nn.Sequential(*[TransformerBlock(dim=int(n_channels), num_heads=1, ffn_expansion_factor=2.66,
-                         bias=False, LayerNorm_type='WithBias') for i in range(1)])
+        self.resblock_da = DoubleAttentionLayer(n_channels, n_channels, n_channels)
+        # self.resblock_da = nn.Sequential(*[TransformerBlock(dim=int(n_channels), num_heads=1, ffn_expansion_factor=2.66,
+        #                  bias=False, LayerNorm_type='WithBias') for i in range(1)])
         self.resblock_se = SELayer(n_channels)
         self.resblock_sa = sa_layer(n_channels, 16)
         # self.conv3 = nn.Conv2d(n_channels, n_channels, kernel_size=3, stride=1, padding=1)
+        self.resblock_trans_normal = nn.Sequential(*[TransformerBlock(dim=int(n_channels), num_heads=1, ffn_expansion_factor=2.66,
+                         bias=False, LayerNorm_type='WithBias') for i in range(1)])
+        self.resblock_trans_dual = nn.Sequential(
+            *[TransformerBlock_dual(dim=int(n_channels), num_heads=1, ffn_expansion_factor=2.66,
+                               bias=False, LayerNorm_type='WithBias') for i in range(1)])
+        self.resblock_trans_eca = nn.Sequential(
+            *[TransformerBlock_eca(dim=int(n_channels), num_heads=1, ffn_expansion_factor=2.66,
+                                    bias=False, LayerNorm_type='WithBias') for i in range(1)])
+        self.resblock_trans_sa = nn.Sequential(
+            *[TransformerBlock_sa(dim=int(n_channels), num_heads=1, ffn_expansion_factor=2.66,
+                                    bias=False, LayerNorm_type='WithBias') for i in range(1)])
+        self.resblock_trans_sge = nn.Sequential(
+            *[TransformerBlock_sge(dim=int(n_channels), num_heads=1, ffn_expansion_factor=2.66,
+                                    bias=False, LayerNorm_type='WithBias') for i in range(1)])
 
     def forward(self, x, select):
         if select == 0:
--- underwater_model/restormer.py
+++ underwater_model/restormer.py
@@ -127,7 +127,7 @@ class Attention(nn.Module):
 
 # ECA attention module
 class Attention_eca(nn.Module):
-    def __init__(self, k_size, num_heads, bias):
+    def __init__(self, num_heads, k_size, bias):
         super(Attention_eca, self).__init__()
         self.num_heads = num_heads
 
@@ -148,6 +148,150 @@ class Attention_eca(nn.Module):
         output = torch.cat(outputs, dim=1)
 
         return output
+
+
+# SGE attention module
+class Attention_sge(nn.Module):
+    def __init__(self, num_heads, groups):
+        super(Attention_sge, self).__init__()
+        self.num_heads = num_heads
+
+        self.groups = groups
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.weight = nn.Parameter(torch.zeros(1, groups, 1, 1))
+        self.bias = nn.Parameter(torch.ones(1, groups, 1, 1))
+        self.sig = nn.Sigmoid()
+
+    def forward(self, x):
+        heads = x.chunk(self.num_heads, dim=1)
+        outputs = []
+        for head in heads:
+            b, c, h, w = head.size()
+            # print(x.shape)
+            head = head.reshape(b * self.groups, -1, h, w)
+            xn = head * self.avg_pool(head)
+            xn = xn.sum(dim=1, keepdim=True)
+            t = xn.reshape(b * self.groups, -1)
+            t = t - t.mean(dim=1, keepdim=True) + 1e-5
+            std = t.std(dim=1, keepdim=True) + 1e-5
+            t = t / std
+            t = t.reshape(b, self.groups, h, w)
+            t = t * self.weight + self.bias
+            t = t.reshape(b * self.groups, 1, h, w)
+            head = head * self.sig(t)
+            head = head.reshape(b, c, h, w)
+            outputs.append(head)
+
+        # Two different branches of ECA module
+        output = torch.cat(outputs, dim=1)
+
+        return output
+
+
+class Attention_sa(nn.Module):
+    def __init__(self, num_heads, groups, channel):
+        super(Attention_sa, self).__init__()
+        self.num_heads = num_heads
+
+        self.groups = groups
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.cweight = nn.Parameter(torch.zeros(1, channel // (2 * groups), 1, 1))
+        self.cbias = nn.Parameter(torch.ones(1, channel // (2 * groups), 1, 1))
+        self.sweight = nn.Parameter(torch.zeros(1, channel // (2 * groups), 1, 1))
+        self.sbias = nn.Parameter(torch.ones(1, channel // (2 * groups), 1, 1))
+
+        self.sigmoid = nn.Sigmoid()
+        self.gn = nn.GroupNorm(channel // (2 * groups), channel // (2 * groups))
+
+    @staticmethod
+    def channel_shuffle(x, groups):
+        b, c, h, w = x.shape
+
+        x = x.reshape(b, groups, -1, h, w)
+        x = x.permute(0, 2, 1, 3, 4)
+
+        # flatten
+        x = x.reshape(b, -1, h, w)
+
+        return x
+
+    def forward(self, x):
+        heads = x.chunk(self.num_heads, dim=1)
+        outputs = []
+        for head in heads:
+            b, c, h, w = head.shape
+
+            head = head.reshape(b * self.groups, -1, h, w)
+            x_0, x_1 = head.chunk(2, dim=1)
+
+            # channel attention
+            xn = self.avg_pool(x_0)
+            xn = self.cweight * xn + self.cbias
+            xn = x_0 * self.sigmoid(xn)
+
+            # spatial attention
+            xs = self.gn(x_1)
+            xs = self.sweight * xs + self.sbias
+            xs = x_1 * self.sigmoid(xs)
+
+            # concatenate along channel axis
+            out = torch.cat([xn, xs], dim=1)
+            out = out.reshape(b, -1, h, w)
+
+            out = self.channel_shuffle(out, 2)
+
+            outputs.append(out)
+
+        # Two different branches of ECA module
+        output = torch.cat(outputs, dim=1)
+
+        return output
+
+class Attention_dual(nn.Module):
+    def __init__(self, num_heads, in_channels: int, c_m: int, c_n: int, reconstruct = False):
+        super(Attention_dual, self).__init__()
+        self.num_heads = num_heads
+
+        self.c_m = c_m
+        self.c_n = c_n
+        self.in_channels = in_channels
+        self.reconstruct = reconstruct
+        self.convA = nn.Conv2d(in_channels, c_m, kernel_size=1)
+        self.convB = nn.Conv2d(in_channels, c_n, kernel_size=1)
+        self.convV = nn.Conv2d(in_channels, c_n, kernel_size=1)
+        if self.reconstruct:
+            self.conv_reconstruct = nn.Conv2d(c_m, in_channels, kernel_size=1)
+
+    def forward(self, x):
+        heads = x.chunk(self.num_heads, dim=1)
+        outputs = []
+        for head in heads:
+            batch_size, c, h, w = head.size()
+            assert c == self.in_channels, 'input channel not equal!'
+            A = self.convA(head)  # (B, c_m, h, w) because kernel size is 1
+            B = self.convB(head)  # (B, c_n, h, w)
+            V = self.convV(head)  # (B, c_n, h, w)
+            tmpA = A.view(batch_size, self.c_m, h * w)
+            attention_maps = B.view(batch_size, self.c_n, h * w)
+            attention_vectors = V.view(batch_size, self.c_n, h * w)
+            attention_maps = F.softmax(attention_maps, dim=-1)  # softmax on the last dimension to create attention maps
+            # step 1: feature gathering
+            global_descriptors = torch.bmm(tmpA, attention_maps.permute(0, 2, 1))  # (B, c_m, c_n)
+            # step 2: feature distribution
+            attention_vectors = F.softmax(attention_vectors, dim=1)  # (B, c_n, h * w) attention on c_n dimension
+            tmpZ = global_descriptors.matmul(attention_vectors)  # B, self.c_m, h * w
+            tmpZ = tmpZ.view(batch_size, self.c_m, h, w)
+            if self.reconstruct: tmpZ = self.conv_reconstruct(tmpZ)
+
+            outputs.append(tmpZ)
+
+        # Two different branches of ECA module
+        output = torch.cat(outputs, dim=1)
+
+        return output
+
+
+
 
 ##########################################################################
 class TransformerBlock(nn.Module):
@@ -320,6 +464,6 @@ if __name__ == '__main__':
     # model2 = nn.Sequential(*[
     #     TransformerBlock(dim=int(48), num_heads=4, ffn_expansion_factor=2.66,
     #                      bias=False, LayerNorm_type='WithBias') for i in range(4)])
-    model3 = Attention_eca(3, 4, False)
+    model3 = Attention_sa(1, 16, 48)
     output2 = model3(input)
     print(output2.shape)
