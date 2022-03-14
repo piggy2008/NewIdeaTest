eb04bde2fd923b13bc0ed854fcf61707a86f10d8
piggy2008 <piggy2008>
2022-03-14 12:18:58 +0900

    train 5k data

--- water_dataset.py
+++ water_dataset.py
@@ -87,7 +87,7 @@ class WaterImage2Folder(data.Dataset):
     def __getitem__(self, index):
         img = Image.open(os.path.join(self.root, 'input', self.imgs[index])).convert('RGB')
         target = Image.open(os.path.join(self.root, 'expertC_gt', self.labels[index])).convert('RGB')
-        print(self.imgs[index], '--', self.labels[index])
+        # print(self.imgs[index], '--', self.labels[index])
         img_list = []
         gt_list = []
         img_list.append(img)
