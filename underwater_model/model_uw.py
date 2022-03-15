436bc714ffd40cb2de38beafbcece1e692025098
piggy2008 <piggy2008>
2022-03-15 18:24:26 +0900

    training subnet

---
+++ underwater_model/model_uw.py
@@ -0,0 +1,86 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+# from underwater_model.base import Base
+from underwater_model.base_uw import Base
+# from underwater_model.base_dark import Base
+from underwater_model.search_net_uw import Search
+from underwater_model.trans_block_dual import TransformerBlock_dual
+
+class Water(nn.Module):
+    def __init__(self, dim):
+        super(Water, self).__init__()
+
+        self.base = Base(dim)
+
+        self.align1 = nn.Sequential(nn.Conv2d(dim * 2, dim, kernel_size=3, stride=1, padding=1),
+                                       nn.ReLU(inplace=True))
+
+        self.align2 = nn.Sequential(nn.Conv2d((dim * 2 ** 1) * 2, (dim * 2 ** 1), kernel_size=3, stride=1, padding=1),
+                                    nn.ReLU(inplace=True))
+
+        self.align3 = nn.Sequential(nn.Conv2d((dim * 2 ** 2) * 2, (dim * 2 ** 2), kernel_size=3, stride=1, padding=1),
+                                    nn.ReLU(inplace=True))
+
+        self.align4 = nn.Sequential(nn.Conv2d((dim * 2 ** 3) * 2, (dim * 2 ** 3), kernel_size=3, stride=1, padding=1),
+                                    nn.ReLU(inplace=True))
+
+        self.search = Search(dim)
+
+        self.refine = nn.Sequential(
+            *[TransformerBlock_dual(dim=int(dim * 2 ** 1), num_heads=2, ffn_expansion_factor=2.66,
+                               bias=False, LayerNorm_type='WithBias') for i in range(1)])
+
+        self.de_predict = nn.Sequential(nn.Conv2d(dim * 2 ** 1, 3, kernel_size=1, stride=1))
+
+        self.de_predict_rgb = nn.Sequential(nn.Conv2d(dim * 2 ** 3, 3, kernel_size=1, stride=1))
+
+        self.de_predict_lab = nn.Sequential(nn.Conv2d(dim * 2 ** 3, 3, kernel_size=1, stride=1))
+
+    def forward(self, rgb, lab, select):
+
+        # first_rgb, first_lab, second_rgb, second_lab, third_rgb, \
+        # third_lab = self.base(rgb, lab, select[:6])
+        # select = [5, 2, 6, 0, 5, 1, 0, 7, 5, 5, 3, 7]
+        level1_rgb, level2_rgb, level3_rgb, level4_rgb, \
+        level1_lab, level2_lab, level3_lab, level4_lab = self.base(rgb, lab)
+
+        # inter_rgb = F.interpolate(self.de_predict_rgb(third_rgb), rgb.size()[2:], mode='bilinear')
+        # inter_lab = F.interpolate(self.de_predict_lab(third_lab), lab.size()[2:], mode='bilinear')
+
+        level1 = self.align1(torch.cat([level1_rgb, level1_lab], dim=1))
+        # first = self.align1(first_lab)
+        level2 = self.align2(torch.cat([level2_rgb, level2_lab], dim=1))
+        # second = self.align2(second_lab)
+        level3 = self.align3(torch.cat([level3_rgb, level3_lab], dim=1))
+        # third = self.align3(third_lab)
+        level4 = self.align4(torch.cat([level4_rgb, level4_lab], dim=1))
+
+        mid_feat = self.search(level4, level3, level2, level1)
+        mid_feat = self.refine(mid_feat)
+
+        mid_rgb = self.de_predict_rgb(level4)
+        mid_lab = self.de_predict_rgb(level4)
+
+        final = self.de_predict(mid_feat)
+
+
+        return final, mid_rgb, mid_lab
+
+if __name__ == '__main__':
+    a = torch.zeros(2, 3, 256, 256)
+    b = torch.zeros(2, 128, 1, 1)
+
+    model = Water(48)
+    # r = model(a, a, [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5])
+    # print(r1.shape, '--', r2.shape, '--', r3.shape)
+    # c = torch.zeros(2, 128, 16, 16)
+    # global_rct = GlobalRCT(128, 128, 8)
+    from torchstat import stat
+
+    stat(model, (3, 256, 256))
+
+    # print(r.shape)
+
+
+
---
+++ underwater_model/search_net_uw.py
@@ -0,0 +1,60 @@
+import torch
+from underwater_model.op import *
+from underwater_model.choice_block import block_choice
+
+from underwater_model.restormer import TransformerBlock
+
+from underwater_model.trans_block_eca import TransformerBlock_eca
+from underwater_model.trans_block_sa import TransformerBlock_sa
+from underwater_model.trans_block_sge import TransformerBlock_sge
+
+class Search(nn.Module):
+    def __init__(self, dim):
+        super(Search, self).__init__()
+        # round 1(1~3), level 1(1~3), op:1~12
+        self.conv_up3 = nn.Sequential(nn.Conv2d((dim * 2 ** 3), (dim * 2 ** 3) * 2, kernel_size=3, stride=1, padding=1, bias=False),
+                                       nn.PixelShuffle(2))
+
+        self.conv_up2 = nn.Sequential(nn.Conv2d((dim * 2 ** 2), (dim * 2 ** 2) * 2, kernel_size=3, stride=1, padding=1, bias=False),
+                                      nn.PixelShuffle(2))
+        self.conv_up1 = nn.Sequential(nn.Conv2d((dim * 2 ** 1), (dim * 2 ** 1) * 2, kernel_size=3, stride=1, padding=1, bias=False),
+                                      nn.PixelShuffle(2))
+
+        self.conv_cat3 = nn.Conv2d(int(dim*2**3), int(dim*2**2), kernel_size=1, bias=False)
+        self.conv_cat2 = nn.Conv2d(int(dim * 2 ** 2), int(dim * 2 ** 1), kernel_size=1, bias=False)
+
+        self.decoder_block3 = nn.Sequential(
+            *[TransformerBlock_sa(dim=int(dim * 2 ** 2), num_heads=2, ffn_expansion_factor=2.66,
+                                    bias=False, LayerNorm_type='WithBias') for i in range(1)])
+        self.decoder_block2 = DoubleAttentionLayer(dim * 2 ** 1, dim * 2 ** 1, dim * 2 ** 1)
+        self.decoder_block1 = nn.Sequential(*[TransformerBlock(dim=int(dim * 2 ** 1), num_heads=2, ffn_expansion_factor=2.66,
+                         bias=False, LayerNorm_type='WithBias') for i in range(1)])
+
+    def forward(self, level4, level3, level2, level1):
+        # print(select)
+        de_level3 = self.conv_up3(level4)
+        de_level3 = torch.cat([de_level3, level3], 1)
+        de_level3 = self.conv_cat3(de_level3)
+        de_level3 = self.decoder_block3(de_level3)
+
+        de_level2 = self.conv_up2(de_level3)
+        de_level2 = torch.cat([de_level2, level2], 1)
+        de_level2 = self.conv_cat2(de_level2)
+        de_level2 = self.decoder_block2(de_level2)
+
+        de_level1 = self.conv_up1(de_level2)
+        de_level1 = torch.cat([de_level1, level1], 1)
+        de_level1 = self.decoder_block1(de_level1)
+
+        return de_level1
+
+if __name__ == '__main__':
+    level1 = torch.zeros(2, 48, 128, 128)
+    level2 = torch.zeros(2, 96, 64, 64)
+    level3 = torch.zeros(2, 192, 32, 32)
+    level4 = torch.zeros(2, 384, 16, 16)
+
+    model = Search(dim=48)
+    choice = [0, 0, 0]
+    output = model(level4, level3, level2, level1, choice)
+    print(output.size())
